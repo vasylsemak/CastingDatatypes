2057ad59b88d333b828209d7a0cc1e91d4208c44
Vasyl Semak <vasylsmk1401@gmail.com>
2021-06-15 20:52:13 -0400

    create statements that utilize casting to convert one datatype to another

---
+++ src/Example.java
@@ -0,0 +1,22 @@
+
+public class Example {
+
+	public static void main(String[] args) {
+		// initial values
+		int i = 200;
+		
+		// cast to short
+		short s = (short)i;
+		System.out.println("Cast to short: " + s);
+		
+		//cast to a double
+		double d = (double)i;
+		System.out.println("Cast to double: " + d);
+		
+		//cast to a byte
+		byte b = (byte)i;
+		System.out.println("Cast to byte: " + b);
+
+	}
+
+}
